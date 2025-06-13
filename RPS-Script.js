f4c4424fb41da1d3fef11792938ec502b706b34e
Blade21Shade <blade21shade@gmail.com>
2025-06-13 15:34:21 -0500

    Make the getPlayerChoice function
    RPS-Script.js
    - Get the choice from the player with the getPlayerChoice() function
     - The project description said we should assume they enter a valid choice to keep the project simple, so I am doing so
    - .toLowerCase() is used so I can easily compare the choice against the computer later

--- RPS-Script.js
+++ RPS-Script.js
@@ -17,3 +17,20 @@ function getComputerChoice() {
     return choice;
 }
 
+/*
+Get the choice from the player
+
+Get it from some console input
+Check to make sure the choice is one of
+    rock
+    paper
+    scissors
+Return the choice (Odin said we don't need to do re-prompting for incorrect choices, so I won't)
+*/
+function getPlayerChoice() {
+    return prompt("Type which of the three you wish to play: ").toLowerCase();
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(getPlayerChoice());
+}
