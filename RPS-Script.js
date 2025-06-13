85f99493c619be950a8b5d48b22385b87ce3ac12
Blade21Shade <blade21shade@gmail.com>
2025-06-13 16:02:47 -0500

    Make functionality for playing one round
    RPS-Script.js
    - Get rid of the counters for what choice the computer made
     - I forgot to remove this when I got rid of the other testing stuff
    - Make 2 functions for playing a round
     - playRound()
      - The bulk of the logic
      - Gets both the player and computer choices and compares them against each other
      - Passes whoever won and both choices to printWinnerInfo()
    - printWinnerInfo()
     - Prints who won this round and both choices
     - Also prints the total wins for the player, ties, and the computer
      - It also does an empty line at the end for easy reading in the console

--- RPS-Script.js
+++ RPS-Script.js
@@ -11,13 +11,10 @@ function getComputerChoice() {
     // This isn't a perfect 1/3 split for each choice but it is close enough
     if (num > 0.66) {
         choice = "rock";
-        rockCount++;
     } else if (num > 0.33) {
         choice = "paper";
-        paperCount++;
     } else {
         choice = "scissors";
-        scissorsCount++;
     }
 
     return choice;
@@ -25,4 +22,93 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
     return prompt("Type which of the three you wish to play: ").toLowerCase();
+}
+
+// Play functions
+
+/*
+The function for playing a single round
+
+Get the choices from the computer and player
+Compare those against each other
+Whichever wins has their score incremented, or in the case of a tie the tie counter is incremented
+Then an output for who won that round and all score variables
+*/
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+
+    let winner = "";
+
+    switch(playerChoice) {
+        case "rock":
+            if (computerChoice === "rock") {
+                winner = "tie";
+            } else if (computerChoice === "paper") {
+                winner = "computer";
+            } else { // scissors
+                winner = "player";
+            }
+            break;
+        
+        case "paper":
+            if (computerChoice === "rock") {
+                winner = "player";
+            } else if (computerChoice === "paper") {
+                winner = "tie";
+            } else { // scissors
+                winner = "computer";
+            }
+            break;
+
+        case "scissors":
+            if (computerChoice === "rock") {
+                winner = "computer";
+            } else if (computerChoice === "paper") {
+                winner = "player";
+            } else { // scissors
+                winner = "tie";
+            }
+            break;
+    }
+
+    printWinnerInfo(winner, playerChoice, computerChoice);
+}
+
+/*
+The function for printing the winner and statistics for wins
+
+Prints who won this round and the current statistics for wins
+    Who won
+    Player wins
+    Ties
+    Computer wins
+*/
+
+function printWinnerInfo(roundWinner, playerChoice, computerChoice) {
+    // Print who won this round
+    if (roundWinner === "player") {
+        console.log(`Player wins! ${playerChoice} beats ${computerChoice}!`);
+        playerScore++;
+    } else if (roundWinner === "computer") {
+        console.log(`Computer wins! ${computerChoice} beats ${playerChoice}!`);
+        computerScore++;
+    } else { // tie
+        console.log(`Tie! Player chose ${playerChoice} and computer chose ${computerChoice}!`);
+        tieScore++;
+    }
+
+    // Print statistics
+    console.log(`  Player wins: ${playerScore}`);
+    console.log(`         Ties: ${tieScore}`);
+    console.log(`Computer Wins: ${computerScore}`);
+    console.log("");
+}
+
+
+
+// Testing for 1 round (this will test multiple however)
+for (let i = 0; i < 12; i++) {
+    playRound();
 }
