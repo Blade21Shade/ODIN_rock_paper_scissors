15230e138f8220ff10a593a4c7325ab3ac8845d8
Blade21Shade <blade21shade@gmail.com>
2025-06-13 15:10:06 -0500

    Make getComputerChoice
    RPS-Script.js
    - Plan, make, and test the getComputerChoice function
    - The for loop and the console.log() were for testing purposes to see how close to 1/3 the results are; generally rock is more common than the other two, but not by a crazy amount
     - Also in a small sample size of games, such as 30, it looked basically random

--- RPS-Script.js
+++ RPS-Script.js
@@ -1,1 +1,40 @@
-console.log("Hello!");
+
+/*
+Get the computer's choice
+
+This will get the choice of the computer
+This will be done by getting a random number, 1-3
+ 1 will be rock
+ 2 will be paper
+ 3 will be scissors
+Then the function will return the choice as a string
+*/
+
+let rockCount = 0;
+let paperCount = 0;
+let scissorsCount = 0;
+
+function getComputerChoice() {
+    let num = Math.random();
+    
+    let choice;
+    // This isn't a perfect 1/3 split for each choice but it is close enough
+    if (num > 0.66) {
+        choice = "rock";
+        rockCount++;
+    } else if (num > 0.33) {
+        choice = "paper";
+        paperCount++;
+    } else {
+        choice = "scissors";
+        scissorsCount++;
+    }
+
+    return choice;
+}
+
+for (let i = 0; i < 3000000; i++) {
+    getComputerChoice();
+}
+
+console.log(rockCount, paperCount, scissorsCount);
