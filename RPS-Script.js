a936e5824814e68c3b3a6294b26ba2e4edd5cd9e
Blade21Shade <blade21shade@gmail.com>
2025-06-13 15:17:05 -0500

    Test Github connection
    For some reason my commits in this project aren't getting pushed to Github even though I've set it up just like other projects.
    
    This is a test to see if restarting the VM will fix the issue
    
    RPS-Script.js
    - Get rid of comment and testing logic

--- RPS-Script.js
+++ RPS-Script.js
@@ -1,19 +1,3 @@
-
-/*
-Get the computer's choice
-
-This will get the choice of the computer
-This will be done by getting a random number, 1-3
- 1 will be rock
- 2 will be paper
- 3 will be scissors
-Then the function will return the choice as a string
-*/
-
-let rockCount = 0;
-let paperCount = 0;
-let scissorsCount = 0;
-
 function getComputerChoice() {
     let num = Math.random();
     
@@ -33,8 +17,3 @@ function getComputerChoice() {
     return choice;
 }
 
-for (let i = 0; i < 3000000; i++) {
-    getComputerChoice();
-}
-
-console.log(rockCount, paperCount, scissorsCount);
